3ea1cefd255f2e90886b91b49f5bdada9eb0b9ab
Krishna Sah <kanhaiyasah7700@.com>
2023-12-27 22:46:58 +0545

    login

--- resources/views/frontend/common/header.blade.php
+++ resources/views/frontend/common/header.blade.php
@@ -3,17 +3,17 @@
 
       <div class="logo">
         <h1><a href="index.html">Roomie</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+        <!-- Uncomment below if you prefer to use an image logo
+        <a href="index.html"><img src="{{ asset('frontend/img/logo.png') }}" alt="kkhkj" class="img-fluid"></a> -->
       </div>
 
       <nav id="navbar" class="navbar">
         <ul>
-          <li><a class="nav-link scrollto active" href="#hero">Home</a></li>
+          <li><a class="nav-link scrollto active" href="{{ url('/') }}">Home</a></li>
           <li><a class="nav-link scrollto" href="#services">Services</a></li>
           <li><a class="nav-link scrollto" href="#team">Team</a></li>
           <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
-          <li><a class="nav-link scrollto active" href="#hero">Login / Register</a></li>
+          <li><a class="nav-link scrollto " href="{{ route('login.page') }}">Login / Register</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
       </nav><!-- .navbar -->
