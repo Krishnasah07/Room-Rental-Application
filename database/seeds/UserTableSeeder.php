2d79ddc848d7441d638b89d6f5be329ba7ef32df
Krishna Sah <kanhaiyasah7700@.com>
2023-12-28 23:15:43 +0545

    deleted folders nd files recovery

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [
+            'role' => 'Admin',
+            'name' => 'Krishna',
+            'email' => 'krishna@gmail.com',
+            'password' => bcrypt('123456')
+        ]; 
+        DB::table('users')->insert($data);      
+    }
+}
