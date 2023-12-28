10ff0cfc0e90db0ab12ad13b88cd83aa189b346b
Krishna Sah <kanhaiyasah7700@.com>
2023-12-28 23:17:05 +0545

    deleted folders nd files recovery

--- routes/web.php
+++ routes/web.php
@@ -4,17 +4,21 @@ Route::get('/', function (){
     return view('frontend.index');
 });
 
+// login and Register Route
 Route::view('/login','login')->name('login.page');
 Route::post('/submit','LoginController@login')->name('admin.login.submit');
 
+// group of admin routes
 Route::group(['prefix'=>'admin'],function(){
     Route::view('dashboard','backend.dashboard.admin.index')->name('admin.dashboard');
 });
 
+// group of renter routes
 Route::group(['prefix'=>'renter'],function(){
     Route::view('dashboard','backend.dashboard.renter.index')->name('renter.dashboard');
 });
 
+// group of landlord routes
 Route::group(['prefix'=>'landlord'],function(){
     Route::view('dashboard','backend.dashboard.landlord.index')->name('landlord.dashboard');
 });
